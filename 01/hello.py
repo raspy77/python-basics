5eed566b252f3077fcd11c39cc4ed97dc9db77bb
raspy77 <shk9339@naver.com>
2020-08-26 16:07:41 +0900

    들여쓰기연습

---
+++ 01/hello.py
@@ -0,0 +1,23 @@
+
+
+print('hello world')
+
+h= 'hello world'
+
+print(h)
+
+
+a = 1
+if a < 2:
+    print(a)
+    print('big')
+
+b= 20
+
+def my_func(i):
+    if i > 10:
+        print('big')
+    else:
+        print('small')
+
+my_func(20)
